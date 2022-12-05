a7c8779e531d4082977ccd53636fdd4fdf372b93
Gareth Davies <mrgarethddavies@gmail.com>
2022-12-05 11:33:01 +0100

    all index components connected

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,25 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Splash from "./components/Splash";
 import Nav from "./components/Nav";
+import Headliners from "./components/Headliners";
+import Lineup from "./components/Lineup";
+import Footer from "./components/Footer";
+
 
 export default function Home() {
-  return <>
+  return (
+  <>
+  <div>
+    <Head>
+    <title>The Official FootFest Hompepage</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    </div>
   <Nav/>
   <Splash />
   <Header/>
   <Headliners />
   <Lineup/>
   <Footer/>
-
-  </>;
+  </>);
 }
