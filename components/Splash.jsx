5833de0f9cf354e4fc95c2554c8f9b45177af077
Gareth Davies <mrgarethddavies@gmail.com>
2022-12-07 17:36:24 +0100

    init

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Splash() {
   return (
-    <section>
+    <section className="SplashContainer">
       <h1>The best damned festival</h1>
+      <h1>28:13:45:14</h1>
     </section>
   );
 }
