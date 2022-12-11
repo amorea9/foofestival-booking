e48c3ec36e219079d3b76c9d3bbdaf2b16fb77d4
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-12-11 13:31:58 +0100

    order overview + buttons basic styling

--- components/OrderOverview.jsx
+++ components/OrderOverview.jsx
@@ -2,43 +2,43 @@ import React from "react";
 
 function OrderOverview() {
   return (
-    <div>
+    <div className="order-overview">
       <h3>Your order</h3>
       <h5>Your tickets are reserved for 5 minutes.</h5>
-      <section>
-        <div>
+      <section className="order-overview-tickets">
+        <div className="overview-item ticket">
           <h4>Regular ticket</h4>
           <h4>Amount</h4>
           <h4>Price</h4>
         </div>
-        <div>
+        <div className="overview-item ticket">
           <h4>Regular ticket</h4>
           <h4>Amount</h4>
           <h4>Price</h4>
         </div>
       </section>
-      <section>
+      <section className="order-overview-camp">
         <h4>Camp name</h4>
       </section>
-      <section>
-        <div>
+      <section className="order-overview-addons">
+        <div className="overview-item">
           <h4>Tent set up</h4>
           <h4>Price</h4>
         </div>
-        <div>
+        <div className="overview-item">
           <h4>Green camping</h4>
           <h4>Price</h4>
         </div>
       </section>
-      <section>
+      <section className="order-overview-fee overview-item">
         <h4>Booking fee</h4>
         <h4>x1</h4>
         <h4>99,-</h4>
       </section>
-      <div>
+      <section className="order-overview-total">
         <h4>Total</h4>
         <h4>XXX,-</h4>
-      </div>
+      </section>
     </div>
   );
 }
