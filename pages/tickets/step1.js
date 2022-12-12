29ef8354204d4fc6e6b5deb41d996fe57b3ecbed
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-12-12 17:55:20 +0100

    storing user order in state and functions for updating it

--- pages/tickets/step1.js
+++ pages/tickets/step1.js
@@ -10,15 +10,25 @@ import Button from "../../components/UI-components/Button";
 // 3. if VIP + REGULAR > 5 then block counting function
 
 function step1(props) {
-  console.log(props)
+  console.log(props);
+
+  // // update tickets in order overview based on user input
+  // function updateTickets(event) {
+  //   let tick = event.target.value;
+  //   setTickets(tick);
+  //   console.log(tick);
+  // }
+
+  // function setTickets(ticketAmount, vipAmount, regAmount)
+
   return (
     <>
       <section className="order-interface">
         <StepIndicator />
         <h2>Select your tickets</h2>
         <h5>Note: There is a limit of 5 tickets per customer.</h5>
-        <InputCounter title={"Regular"} name={"RegTicket"} subtitle={"All days"} description={"Access to concerts and vendors."} price={"799,-"}/>
-        <InputCounter title={"VIP"} name={"VIPTicket"} subtitle={"All days"} description={"Includes food & drink discounts and early access to stages."} price={"1299,-"}/>
+        <InputCounter title={"Regular"} name={"RegTicket"} subtitle={"All days"} description={"Access to concerts and vendors."} price={"799,-"} setTickets={props.setTickets} />
+        <InputCounter title={"VIP"} name={"VIPTicket"} subtitle={"All days"} description={"Includes food & drink discounts and early access to stages."} price={"1299,-"} />
         <h2>Select your camp area</h2>
         <h5>Each ticket counts for one space in a camp area.</h5>
         <InputDrop optionOne={"Area 1"} optionTwo={"Area 2"} />
