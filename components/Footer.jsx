17e1294f0436d5d0f6efe795c489256685f2d5a3
Gareth Davies <mrgarethddavies@gmail.com>
2022-12-13 18:11:27 +0100

    footer populated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import tiktok from "../public/icons/tiktok_icon.png";
+import facebook from "../public/icons/facebook_icon.png";
+import insta from "../public/icons/insta_icon.png";
+import Image from "next/image";
+//continue importing icons and get the working!
 
 function Footer() {
   return (
     <footer>
-      <h3>Contact</h3>
-      <div>Social media icons</div>
+      <div>
+        <a href="#"><Image src={tiktok} alt="Foofest Tiktok Page" className="icon"/></a>
+        <a href="#"><Image src={insta} alt="Foofest Instagram Page" className="icon"/></a>
+        <a href="#"><Image src={facebook} alt="Foofest Facebook Page" className="icon"/></a>
+      </div>
+      <h5>FAQs</h5>
+      <h5>Volunteer</h5>
+      <h5>Merch</h5>
+      <h5>Contact</h5>
     </footer>
   );
 }
