adf310b25145540c2e8f3b6d178d4e89dd778265
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-12-16 12:57:27 +0100

    checkbox styling, bit buggy

--- components/UI-components/InputCheck.jsx
+++ components/UI-components/InputCheck.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
 
-let setUp = false
-let green = false
+let setUp = false;
+let green = false;
 
 function InputCheck(props) {
-    function handleChange(event) {
-        if (props.name === "TentSetup") {
-           setUp = event.target.checked
-        } else if (props.name === "TentGreen") {
-           green = event.target.checked
-        }
-        props.setOptions(setUp, green)
+  function handleChange(event) {
+    if (props.name === "TentSetup") {
+      setUp = event.target.checked;
+    } else if (props.name === "TentGreen") {
+      green = event.target.checked;
     }
-    return (
-        <div className="input-box box">
-        <div className='input-text'>
-          <h3>{props.title}</h3>
-          <h4>{props.subtitle}</h4>
-          <p>{props.description}</p>
-        </div>
-        <div>
-          <h3>{props.price}</h3>
-          {/* insert input type depending on props.type */}
-          <input type="checkbox"  onChange={handleChange}/>
+    props.setOptions(setUp, green);
+  }
+  return (
+    <div className="input-box box">
+      <div className="input-text">
+        <h3>{props.title}</h3>
+        <h4>{props.subtitle}</h4>
+        <p>{props.description}</p>
+      </div>
+      <div>
+        <h3>{props.price}</h3>
+        <div className="checkbox-container">
+          <input type="checkbox" className="hidden" id="checkbox" onChange={handleChange} />
+          <label htmlFor="checkbox" className="checkbox-label"></label>
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
-export default InputCheck
+export default InputCheck;
